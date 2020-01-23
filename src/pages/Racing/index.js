0bfe58adb7384a8211484a802860fa42756635f8
mayconline <mayconline.ti@gmail.com>
2020-01-22 22:34:55 -0300

    fix message initial racing

--- src/pages/Racing/index.js
+++ src/pages/Racing/index.js
@@ -2,23 +2,52 @@ import React , {useState, useEffect, useCallback} from 'react';
 
 import {Pista, Carro, Mensagem, Container, Nickname} from './styled';
 
+
 export default function Racing(){
     const [position, setPosition] = useState(300);
     const [starting, setStarting] = useState(false);
-    const [message, setMessage] = useState('3, 2 , 1 ...');
+    const [message, setMessage] = useState('');
     const [nickname, setNickname] = useState('');
-  
+   
+    
 
     useEffect(()=>{
        setNickname(localStorage.getItem('@nickname')) 
     },[])
    
     useEffect(()=>{
+      
+            setMessage('3 ...')                 
+
         setTimeout(()=>{
-            setStarting(true);         
+            setMessage('2 ...')                 
+        },1000)
+
+        setTimeout(()=>{
+            setMessage('1 ...')                 
+        },2000)
+
+        setTimeout(()=>{
+            setMessage('GO !')    
+                         
         },3000)
+
+
+        setTimeout(()=>{
+           setStarting(true);         
+        },3300)
     },[])
+
+
+
+
   
+    useEffect(()=>{
+        setTimeout(()=>{
+            setMessage('Congrulation Wins')
+            setStarting(false);         
+        },60000)
+    },[])
 
     
     const handlePosition = useCallback( (event)=> {
@@ -52,14 +81,13 @@ export default function Racing(){
           };
     },[handlePosition])
 
+   
 
-    
 
     return(
         <>
-           
         <Container>
-           
+        
             <Pista starting={starting} >
             <Nickname>{nickname && nickname}</Nickname>
                 <Mensagem >{!starting && message }</Mensagem>
