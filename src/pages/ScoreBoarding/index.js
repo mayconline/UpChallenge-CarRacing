75bea5638cf6fc6e659ee0bd073d8ce198a63682
mayconline <mayconline.ti@gmail.com>
2020-01-26 14:14:42 -0300

    fix scoreboarding

--- src/pages/ScoreBoarding/index.js
+++ src/pages/ScoreBoarding/index.js
@@ -1,34 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+
+import {useQuery} from '@apollo/react-hooks'
+import {gql} from 'apollo-boost';
 
 import {Container, StoryContainer, Score, Button} from './styled';
 
+const USER_QUERY = gql`
+    query {
+        users{
+            _id,
+            name,
+            score
+        }
+    }
+
+`
+
 export default function ScoreBoarding({history}){
    
-    const [scoreRanking, setScoreRaking] = useState([])
-   
-    useEffect(()=>{
-        const response = localStorage.getItem('@score')
-            const parseJsonResponse = JSON.parse(response)
-       
-        setScoreRaking([parseJsonResponse, ...scoreRanking])
-    },[])
+  
+    const {loading, error, data} = useQuery(USER_QUERY)
 
  
     return(
         <Container>
             <StoryContainer>
                 <strong>Score Boarding</strong>
-                <Score>
-                    {scoreRanking && 
-                       scoreRanking.map(score => (
-                            
-                              <li key={score._id}>
-                              {score.nicknameValue } : {score.scoreValue}
-                              </li>
+                <Score>        
+                    {loading && <p>loading ...</p>}
+                    {error && <p>Houve um erro ao trazer os dados</p>}
 
-                       )) 
-            
-                    }
+                        {data && data.users.map(user => (
+                                        
+                                        <li key={user._id}>
+                                        {user.name } : {user.score}
+                                        </li>
+
+                                )) 
+                        
+                         }
                 </Score>
                 <Button onClick={()=>{history.push(`/`)}}>Voltar</Button>
             </StoryContainer>
--- src/pages/Teste/index.js
+++ src/pages/Teste/index.js
@@ -3,35 +3,15 @@ import React from 'react';
 import {useQuery} from '@apollo/react-hooks'
 import {gql} from 'apollo-boost';
 
-const USER_QUERY = gql`
-    query {
-        users{
-            _id,
-            name,
-            score
-        }
-    }
-
-`
 
 export default function Teste(){
 
-        const {loading, error, data} = useQuery(USER_QUERY)
+     
     
     return(
        <div>
 
-           {loading && <p>loading ...</p>}
-            {error && <p>Houve um erro ao trazer os dados</p>}
-
-            {data && data.users.map(user=>(
-                <ul key={user._id}>
-                    <li>
-                        {user.name} : {user.score}
-                    </li>
-                </ul>
-            ))}
-          
+         tste
        </div>
     )
 
